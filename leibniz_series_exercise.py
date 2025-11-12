6db71c22e690a592b9a98810d194ed6f66c2a8b5
1tsZak <148216415+1tsZak@users.noreply.github.com>
2025-11-12 09:39:00 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,8 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    lst=[]
+    for i in range(n_terms):
+        if i%2==0:
+            lst.append(1/(i*2+1))
+        else:
+            lst.append(-1/(i*2+1))
+    return 4*sum(lst)
